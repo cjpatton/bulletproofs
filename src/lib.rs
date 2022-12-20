5fdff8d55ad4cf3cc59f9bfeb67f5ed5455cb13f
Christopher Patton <chrispatton@gmail.com>
2022-12-20 14:46:49 -0800

    Move `inner_product` to public API

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,7 @@ mod transcript;
 
 pub use crate::errors::ProofError;
 pub use crate::generators::{BulletproofGens, BulletproofGensShare, PedersenGens};
+pub use crate::inner_product_proof::inner_product;
 pub use crate::linear_proof::LinearProof;
 pub use crate::range_proof::RangeProof;
 
